11ca21a3724adb4564f147bd4a1477fcc696c147
Nikita Santalov <na.santalov@yandex.ru>
2022-03-17 22:45:24 +0400

    Inisital commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        //Вводные данные
+        int ticket = 100;
+        int bonus = 20;
+
+        //логика
+        int bonusMiles = ticket / bonus;
+
+        System.out.println("Количество бонсуных милей составляет: " + bonusMiles);
+
+    }
+}
