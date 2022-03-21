edd336bc5d110eaaa5459847517f7e137e2911dc
Nikita Santalov <na.santalov@yandex.ru>
2022-03-21 20:27:31 +0400

    add BonusMilesService

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        //Вводные данные
-        int ticket = 100;
-        int bonus = 20;
-
-        //логика
-        int bonusMiles = ticket / bonus;
-
-        System.out.println("Количество бонсуных милей составляет: " + bonusMiles);
-
+        BonusMilesService service = new BonusMilesService();
+        int miles = service.calculate(10_000);
+        System.out.println(miles);
     }
 }
